06fb99290d21fa65a6596eddf81c2aa0331d9e0f
Tayen15 <tiantiantitik@gmail.com>
2026-02-08 23:36:44 +0700

    fix(footer): correct YouTube link in footer component

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,7 +7,7 @@ export default function Footer() {
                     <p>&copy; {new Date().getFullYear()} PEMIRA IM STT Terpadu <a href="https://nurulfikri.ac.id" className="hover:underline-offset-1 hover:underline transition-colors" target="_blank" rel="noopener noreferrer">Nurul Fikri</a>.</p>
                     <div className="flex items-center gap-6">
                          <Link href="https://www.instagram.com/pemirasttnf/" target="_blank" rel="noopener noreferrer" className="hover:text-primary transition-colors">Instagram</Link>
-                         <Link href="https://www.youtube.com/@KPRSTTNF" target="_blank" rel="noopener noreferrer" className="hover:text-primary transition-colors">Youtube</Link>
+                         <Link href="https://www.youtube.com/@pemirasttnf" target="_blank" rel="noopener noreferrer" className="hover:text-primary transition-colors">Youtube</Link>
                     </div>
                </div>
           </footer>
